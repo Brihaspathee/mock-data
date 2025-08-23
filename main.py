e215c62b9a911bb2a59edcd333ce03533acfbf43
vbalaji215-dev <vbalaji215@outlook.com>
2025-08-23 18:13:48 -0400

    provider_read updated

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from aton_writes.service.aton_write import AtonWrite
 from db import AtonGraphDB, PorticoDB, DBUtils
 from config import settings
 from models.aton import Organization
+from portico_reads.service.provider import provider_read
 from models.portico import Person
 from models.portico.pp_prov import PPProv
-from portico_reads.service.provider.provider_read import ProviderRead
 from transform import transformers
 from transform.transformers import transform_to_aton
 import logging
@@ -12,23 +12,29 @@ import logging
 log = logging.getLogger(__name__)
 
 
+def main():
+    log.info("Starting...")
+    log.info(f"Running on {settings.ENVIRONMENT} environment")
+    log.info(f"POSTGRES info {settings.POSTGRES} environment")
+    log.info(f"NEO4J info {settings.NEO4J} environment")
+    log.info(f"Attributes JSON{settings.FLAT_CONFIG}")
 
+    # Read the providers from Portico
+    portico_db: PorticoDB = PorticoDB()
+    portico_db.connect()
+    with portico_db.get_session() as session:
+        providers = provider_read.read_provider(session)
+    # provider_read: ProviderRead = ProviderRead()
+    # providers: list[PPProv] = provider_read.read_provider()
+    # Transform Providers into the shape that is compatible with Aton
+    organizations: list[Organization] = transform_to_aton(providers)
+    # Iterate through the list of organizations and write them to Aton
+    for organization in organizations:
+        aton_write: AtonWrite = AtonWrite()
+        aton_write.write_to_aton(organization)
 
-log.info("Starting...")
-log.info(f"Running on {settings.ENVIRONMENT} environment")
-log.info(f"POSTGRES info {settings.POSTGRES} environment")
-log.info(f"NEO4J info {settings.NEO4J} environment")
-log.info(f"Attributes JSON{settings.FLAT_CONFIG}")
-
-# Read the providers from Portico
-provider_read: ProviderRead = ProviderRead()
-providers: list[PPProv] = provider_read.read_provider()
-# Transform Providers into the shape that is compatible with Aton
-organizations: list[Organization] = transform_to_aton(providers)
-# Iterate through the list of organizations and write them to Aton
-for organization in organizations:
-    aton_write: AtonWrite = AtonWrite()
-    aton_write.write_to_aton(organization)
+if __name__ == "__main__":
+    main()
 
 
 
--- portico_reads/service/provider/provider_read.py
+++ portico_reads/service/provider/provider_read.py
@@ -1,91 +1,29 @@
+from sqlalchemy import select, Sequence
 from sqlalchemy.orm import joinedload, Session
 
-from db import PorticoDB
 from models.portico import PPProv, PPAddr, PPProvAddr, PPAddrPhones, PPProvAttribValues, PPProvTIN
-from typing import cast
 
 from models.portico.pp_prov_attrib import PPProvAttrib
 import logging
 
 log = logging.getLogger(__name__)
 
-"""
-This module contains the ProviderRead class, which is responsible for reading provider data from the database.
-"""
 
-class ProviderRead:
-    """
-    Handles operations related to reading provider data from the database.
+def read_provider(session:Session) -> list[PPProv] | None:
+    stmt = (
+        select(PPProv)
+        .options(
+            joinedload(PPProv.addresses)
+            .joinedload(PPProvAddr.address)
+            .joinedload(PPAddr.phones)
+            .joinedload(PPAddrPhones.phone),
+            joinedload(PPProv.prov_type),
+            joinedload(PPProv.tin),
+            joinedload(PPProv.attributes).joinedload(PPProvAttrib.attribute_type),
+            joinedload(PPProv.attributes).joinedload(PPProvAttrib.values).joinedload(PPProvAttribValues.field),
+        )
+    )
+    providers: list[PPProv] = list(session.execute(stmt).unique().scalars().all())
+    log.info(f"Read providers successfully:{providers}")
+    return providers
 
-    This class is designed to interface with a database using SQLAlchemy sessions.
-    It initializes a database connection, allows reading of provider information,
-    and ensures proper session and connection management.
-
-    :ivar portico_db: Instance of `PorticoDB` used for managing database connections.
-    :type portico_db: PorticoDB
-    :ivar db_session: The SQLAlchemy session used for database operations.
-    :type db_session: Session
-    """
-    def __init__(self):
-        """
-        A class responsible for initializing and managing the connection to the Portico database.
-
-        The class is designed to handle the setup process for database communication, including
-        instantiating a database object, establishing a connection, and retrieving a session
-        for database interactions.
-
-        Attributes
-        ----------
-        portico_db : PorticoDB
-            Initializes and manages the database connection.
-        db_session : Session
-            Provides a session object for database operations.
-        """
-        log.info("Initializing ProviderRead")
-        self.portico_db: PorticoDB = PorticoDB()
-        self.portico_db.connect()
-        self.db_session: Session = self.portico_db.get_session()
-
-
-    def read_provider(self) -> list[PPProv] | None:
-        """
-        Reads the providers from the database, returns a list of providers, or
-        None if there are issues during the execution. The method also ensures
-        that related data (addresses, phones, and provider types) are pulled
-        using efficient joined loading techniques.
-
-        :return: A list of `PPProv` instances containing detailed provider
-            information, or None if an error occurs during the process.
-        :rtype: list[PPProv] | None
-        """
-
-        log.info("Reading providers")
-        # Read providers from portico database
-        # Eagerly load the provider's addresses, phones and attributes
-        try:
-            providers: list[PPProv] = cast(list[PPProv],
-                self.db_session.query(PPProv)
-                .options(
-                    joinedload(PPProv.addresses)
-                        .joinedload(PPProvAddr.address)
-                        .joinedload(PPAddr.phones)
-                        .joinedload(PPAddrPhones.phone),
-                    joinedload(PPProv.prov_type),
-                    joinedload(PPProv.tin),
-                    joinedload(PPProv.attributes).joinedload(PPProvAttrib.attribute_type),
-                    joinedload(PPProv.attributes).joinedload(PPProvAttrib.values).joinedload(PPProvAttribValues.field)
-                )
-                .all()
-            )
-            # Return the list of providers if successful
-            return providers
-
-        # Handle exceptions during the read process
-        except Exception as e:
-            self.db_session.rollback()
-            log.info("Error: ", e, "\n")
-
-        finally:
-            log.info("Closing DB connections")
-            self.db_session.close()
-            self.portico_db.close()
