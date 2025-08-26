6a0974fc24c67e4cc04c4ccf2790e2b087aa8463
balaji-macbook-air <vbalaji215@outlook.com>
2025-08-26 19:14:14 -0400

    Updated insert_data

--- db_scripts/postgres/data/insert_data.sql
+++ db_scripts/postgres/data/insert_data.sql
@@ -136,7 +136,74 @@ INSERT INTO portown.pp_spec (id,"type",description,site_visit_req) VALUES
 -- 6. Insert data into PP_ADDR table
 INSERT INTO portown.pp_addr (id,"type",addr1,addr2,city,state,zip,county,latitude,longitude,start_date,end_date,fips) VALUES
 	 (1,'BILLING','13377 Batten Lane',NULL,'Odessa','FL','33556','Pasco',NULL,NULL,'2020-01-01',NULL,'12101'),
-	 (2,'MAILING','P.O.8433',NULL,'Odessa','FL','33556','Pasco',NULL,NULL,'2020-01-01',NULL,'12101');
+	 (2,'MAILING','P.O.8433',NULL,'Odessa','FL','33556','Pasco',NULL,NULL,'2020-01-01',NULL,'12101'),
+	 (3,'main','3160 Cedar Ave','Apt 42','Dallas','TX','75203','Dallas County',NULL,NULL,'2019-01-01',NULL,'48113'),
+	 (4,'main','4865 Pine Dr','Apt 37','San Antonio','TX','78205','Bexar County',NULL,NULL,'2019-01-01',NULL,'48029'),
+	 (5,'main','4611 Main Rd','Suite 913','Dallas','TX','75203','Dallas County',NULL,NULL,'2019-01-01',NULL,'48113'),
+	 (6,'main','823 Park Ave','—','Houston','TX','77004','Harris County',NULL,NULL,'2019-01-01',NULL,'48201'),
+	 (7,'main','1998 Park Ln','—','Houston','TX','77004','Harris County',NULL,NULL,'2019-01-01',NULL,'48201'),
+	 (8,'main','9805 Oak Ave','—','Austin','TX','78701','Travis County',NULL,NULL,'2019-01-01',NULL,'48453'),
+	 (9,'main','3064 Lake Dr','Suite 196','Dallas','TX','75201','Dallas County',NULL,NULL,'2019-01-01',NULL,'48113'),
+	 (10,'main','3579 Oak Pl','PO Box 187','Dallas','TX','75203','Dallas County',NULL,NULL,'2019-01-01',NULL,'48113');
+INSERT INTO portown.pp_addr (id,"type",addr1,addr2,city,state,zip,county,latitude,longitude,start_date,end_date,fips) VALUES
+	 (11,'main','361 Elm Blvd','Suite 586','San Antonio','TX','78205','Bexar County',NULL,NULL,'2019-01-01',NULL,'48029'),
+	 (12,'main','1201 Cedar Ln','—','Houston','TX','77003','Harris County',NULL,NULL,'2019-01-01',NULL,'48201'),
+	 (13,'main','5511 Oak St','Apt 23','Houston','TX','77003','Harris County',NULL,NULL,'2019-01-01',NULL,'48201'),
+	 (14,'main','3489 Sunset Pl','Apt 38','San Antonio','TX','78206','Bexar County',NULL,NULL,'2019-01-01',NULL,'48029'),
+	 (15,'main','1268 Maple Blvd','PO Box 365','San Antonio','TX','78207','Bexar County',NULL,NULL,'2019-01-01',NULL,'48029'),
+	 (16,'main','1591 Sunset Ave','Apt 47','Dallas','TX','75202','Dallas County',NULL,NULL,'2019-01-01',NULL,'48113'),
+	 (17,'main','4106 Maple Rd','PO Box 59','Austin','TX','78703','Travis County',NULL,NULL,'2019-01-01',NULL,'48453'),
+	 (18,'main','4645 Maple Pl','—','Rockford','IL','61107','Winnebago County',NULL,NULL,'2019-01-01',NULL,'17201'),
+	 (19,'main','7665 Elm St','—','Rockford','IL','61108','Winnebago County',NULL,NULL,'2019-01-01',NULL,'17201'),
+	 (20,'main','5430 Oak Ave','Suite 718','Springfield','IL','62703','Sangamon County',NULL,NULL,'2019-01-01',NULL,'17167');
+INSERT INTO portown.pp_addr (id,"type",addr1,addr2,city,state,zip,county,latitude,longitude,start_date,end_date,fips) VALUES
+	 (21,'main','3722 Cedar Pl','—','Peoria','IL','61603','Peoria County',NULL,NULL,'2019-01-01',NULL,'17143'),
+	 (22,'main','4139 Maple Rd','Suite 361','Springfield','IL','62702','Sangamon County',NULL,NULL,'2019-01-01',NULL,'17167'),
+	 (23,'main','6543 Elm Rd','Suite 510','Rockford','IL','61107','Winnebago County',NULL,NULL,'2019-01-01',NULL,'17201'),
+	 (24,'main','5097 Main Rd','Suite 302','Rockford','IL','61107','Winnebago County',NULL,NULL,'2019-01-01',NULL,'17201'),
+	 (25,'main','9209 Cedar Ave','PO Box 60','Chicago','IL','60618','Cook County',NULL,NULL,'2019-01-01',NULL,'17031'),
+	 (26,'main','406 Pine Dr','PO Box 59','Peoria','IL','61602','Peoria County',NULL,NULL,'2019-01-01',NULL,'17143'),
+	 (27,'main','9953 Lake Ave','—','Rockford','IL','61108','Winnebago County',NULL,NULL,'2019-01-01',NULL,'17201'),
+	 (28,'main','6397 Oak Ave','Apt 5','Peoria','IL','61603','Peoria County',NULL,NULL,'2019-01-01',NULL,'17143'),
+	 (29,'main','3232 Park Dr','Apt 43','Peoria','IL','61604','Peoria County',NULL,NULL,'2019-01-01',NULL,'17143'),
+	 (30,'main','4290 Pine St','—','Peoria','IL','61604','Peoria County',NULL,NULL,'2019-01-01',NULL,'17143');
+INSERT INTO portown.pp_addr (id,"type",addr1,addr2,city,state,zip,county,latitude,longitude,start_date,end_date,fips) VALUES
+	 (31,'main','4377 Elm Ave','Apt 10','Chicago','IL','60618','Cook County',NULL,NULL,'2019-01-01',NULL,'17031'),
+	 (32,'main','8439 Main Ln','Suite 702','Peoria','IL','61602','Peoria County',NULL,NULL,'2019-01-01',NULL,'17143'),
+	 (33,'main','5791 Hill Rd','—','Macon','GA','31204','Bibb County',NULL,NULL,'2019-01-01',NULL,'13021'),
+	 (34,'main','5861 Oak St','—','Macon','GA','31205','Bibb County',NULL,NULL,'2019-01-01',NULL,'13021'),
+	 (35,'main','9039 Oak St','Apt 4','Savannah','GA','31404','Chatham County',NULL,NULL,'2019-01-01',NULL,'13051'),
+	 (36,'main','3166 Oak Blvd','PO Box 280','Savannah','GA','31401','Chatham County',NULL,NULL,'2019-01-01',NULL,'13051'),
+	 (37,'main','9289 Sunset Dr','Suite 472','Savannah','GA','31401','Chatham County',NULL,NULL,'2019-01-01',NULL,'13051'),
+	 (38,'main','2205 Park Pl','—','Savannah','GA','31405','Chatham County',NULL,NULL,'2019-01-01',NULL,'13051'),
+	 (39,'main','9709 Maple Blvd','Apt 8','Savannah','GA','31404','Chatham County',NULL,NULL,'2019-01-01',NULL,'13051'),
+	 (40,'main','5908 Main Dr','Suite 879','Atlanta','GA','30304','Fulton County',NULL,NULL,'2019-01-01',NULL,'13121');
+INSERT INTO portown.pp_addr (id,"type",addr1,addr2,city,state,zip,county,latitude,longitude,start_date,end_date,fips) VALUES
+	 (41,'main','2861 Hill Rd','—','Augusta','GA','30905','Richmond County',NULL,NULL,'2019-01-01',NULL,'13245'),
+	 (42,'main','2011 Maple Rd','Suite 890','Augusta','GA','30901','Richmond County',NULL,NULL,'2019-01-01',NULL,'13245'),
+	 (43,'main','3899 Pine St','—','Atlanta','GA','30303','Fulton County',NULL,NULL,'2019-01-01',NULL,'13121'),
+	 (44,'main','8189 Sunset Ln','PO Box 144','Savannah','GA','31404','Chatham County',NULL,NULL,'2019-01-01',NULL,'13051'),
+	 (45,'main','2811 Main St','Suite 937','Atlanta','GA','30303','Fulton County',NULL,NULL,'2019-01-01',NULL,'13121'),
+	 (46,'main','5113 Maple Ln','PO Box 204','Augusta','GA','30904','Richmond County',NULL,NULL,'2019-01-01',NULL,'13245'),
+	 (47,'main','2757 Maple Pl','Suite 203','Augusta','GA','30901','Richmond County',NULL,NULL,'2019-01-01',NULL,'13245'),
+	 (48,'main','1011 Oak Ave','PO Box 227','San Francisco','CA','94109','San Francisco County',NULL,NULL,'2019-01-01',NULL,'06075'),
+	 (49,'main','8502 Cedar Blvd','Apt 40','Los Angeles','CA','90029','Los Angeles County',NULL,NULL,'2019-01-01',NULL,'06037'),
+	 (50,'main','9135 Cedar Rd','Apt 25','Sacramento','CA','95814','Sacramento County',NULL,NULL,'2019-01-01',NULL,'06067');
+INSERT INTO portown.pp_addr (id,"type",addr1,addr2,city,state,zip,county,latitude,longitude,start_date,end_date,fips) VALUES
+	 (51,'main','3329 Lake St','Suite 928','San Francisco','CA','94133','San Francisco County',NULL,NULL,'2019-01-01',NULL,'06075'),
+	 (52,'main','8986 Maple Dr','—','Sacramento','CA','95814','Sacramento County',NULL,NULL,'2019-01-01',NULL,'06067'),
+	 (53,'main','8539 Main Ave','Suite 924','Sacramento','CA','95816','Sacramento County',NULL,NULL,'2019-01-01',NULL,'06067'),
+	 (54,'main','7911 Pine Pl','PO Box 165','Los Angeles','CA','90029','Los Angeles County',NULL,NULL,'2019-01-01',NULL,'06037'),
+	 (55,'main','4546 Cedar Dr','Apt 15','Sacramento','CA','95815','Sacramento County',NULL,NULL,'2019-01-01',NULL,'06067'),
+	 (56,'main','741 Oak Ave','Apt 6','San Diego','CA','92103','San Diego County',NULL,NULL,'2019-01-01',NULL,'06073'),
+	 (57,'main','9949 Lake St','—','San Diego','CA','92101','San Diego County',NULL,NULL,'2019-01-01',NULL,'06073'),
+	 (58,'main','3668 Elm Pl','Apt 23','Sacramento','CA','95814','Sacramento County',NULL,NULL,'2019-01-01',NULL,'06067'),
+	 (59,'main','4150 Park Rd','Apt 10','San Francisco','CA','94110','San Francisco County',NULL,NULL,'2019-01-01',NULL,'06075'),
+	 (60,'main','8700 Main Dr','PO Box 241','Los Angeles','CA','90029','Los Angeles County',NULL,NULL,'2019-01-01',NULL,'06037');
+INSERT INTO portown.pp_addr (id,"type",addr1,addr2,city,state,zip,county,latitude,longitude,start_date,end_date,fips) VALUES
+	 (61,'main','1606 Hill St','Apt 20','San Francisco','CA','94109','San Francisco County',NULL,NULL,'2019-01-01',NULL,'06075'),
+	 (62,'main','8257 Oak Ave','—','Sacramento','CA','95814','Sacramento County',NULL,NULL,'2019-01-01',NULL,'06067');
+
 
 -- 7. Insert data into PP_PHONES table
 INSERT INTO portown.pp_phones (id,"type",area_code,exchange,"number") VALUES
